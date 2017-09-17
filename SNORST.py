61d2483268843a6180db5fcec7c65a0b7972020b
Kris Swanson <kris@swannynet.com>
2017-09-17 11:51:43 -0400

    search enhancements
    can 'OR' search on multiple items
    can exclude multiple items from search

--- SNORST.py
+++ SNORST.py
@@ -1,8 +1,14 @@
 """
 SNORST : Serial Number, Order Reporting & Search Tool
 author: Kris Swanson kriswans@cisco.com
-Version: 0.4
+Version: 0.5
 """
+
+import re
+import time
+import datetime
+import sys
+import os
 
 print('\n\n'+64*'*'+'\n\n')
 print("Welcome to the SOF1 SNORST!\n")
@@ -10,11 +16,7 @@ print("(S)erial (N)umber, (O)rder (R)eporting & (S)earch (T)ool")
 print('\n\n'+64*'*'+'\n\n')
 
 def SNORST():
-    import re
-    import time
-    import datetime
-    import sys
-    import os
+
 
     file_list=os.listdir()
     if "Data" in file_list:
@@ -55,7 +57,8 @@ def SNORST():
         print("\nNo file selected. Exiting... ")
         sys.exit()
 
-    print(f_sel_list)
+    print('\n\n...Reading column headers...\n\n')
+
     row_in_list=[]
     rl=[]
     for item in f_sel_list:
@@ -110,72 +113,133 @@ def SNORST():
         sys.exit()
 
     search_val='None'
+    search_input='None'
+    val_list=[]
+    excl_list=[]
+    print("\n\n"+64*'#')
+    print("\n###Build Search INCLUDE List!!!!!!!Build Search INCLUDE List####\n\n")
+    print(64*'#'+'\n\n')
+    print("\nNOTE: Enter Individual search INCLUDE values that will be wildcarded and chained with 'OR' operator\n\n")
+    print("\nNOTE: After INCLUDE List completion, just hit <Enter to continue with building the exclude list\n\n")
+    while search_input != '':
+        search_input=input("\n\nEnter individual search INCLUDE values, then hit <Enter> to continue: ")
+        val_list.append(search_input)
+        print("\nYour INCLUDED search variables will be:\n")
+        for val in val_list:
+            print(val)
 
-    while search_val != '':
-        item=1
-        search_val=input('\nPlease enter search string or <Enter> to exit: ')
-        if search_val != '':
-            ts = time.time()
-            st = datetime.datetime.fromtimestamp(ts).strftime('_%Y-%m-%d_@_%H.%M.%S')
-            rec_filename='report_recurs_'+search_val+'__'+st+'.xls'
-            out=open('report__'+search_val+'__'+st+'.xls','w')
-            rec=open(rec_filename,'w')
-            print("\n\nWriting output to file:"+' report__'+search_val+'__'+st+'.xls\n')
-            print("\n\nWriting output to file:"+' report_recurs_'+search_val+'__'+st+'.xls\n\n')
-            for nums in index_list:
-                out.write(col_dict[int(nums)])
-                out.write('\t')
-                rec.write(col_dict[int(nums)])
-                rec.write('\t')
-            out.write('\n')
-            rec.write('\n')
-
-            while item < len_rl:
-                out_list=[]
-                rs=re.search(search_val,rl[item])
-                if rs != None:
-                    field_list=rl[item].split('\t')
-                    for idx in index_list:
-                        out.write(field_list[int(idx)-1])
-                        out.write("\t")
-                        out_list.append(field_list[int(idx)-1])
-                        len_out_list=len(out_list)
-                    out.write("\n")
-                    rec_search=re.search(search_val,str(out_list))
-                    if rec_search != None:
-                        for thing in out_list:
-                            rec.write(thing)
-                            rec.write('\t')
-                        rec.write("\n")
-                    else:
-                        pass
-
-                else:
-                    pass
-                item+=1
-            out.close()
-            rec.close()
-
-            crunch_file='crunch_'+rec_filename
-            crunch=open(crunch_file,'w')
-            print("\n\nWriting"+crunch_file+"\n\n")
-            dup_file=open(rec_filename,'r')
-            dup_list=dup_file.readlines()
-            crunch.write(dup_list[0].rstrip('\n')+'\tCount/Occurences\n')
-            del dup_list[0]
-            dup_set=set(dup_list)
-            for item in dup_set:
-                crunch.write(item.rstrip('\n')+'\t'+str(dup_list.count(item)))
-                crunch.write('\n')
-            crunch.close()
-            dup_file.close()
+    print("\n\n"+64*'#')
+    print("\n###Build Search EXCLUDE List!!!!!!!Build Search EXCLUDE List####\n\n")
+    print(64*'#'+'\n\n')
+    print("\nNOTE: Enter Individual search EXCLUDE values that will be wildcarded and chained with 'OR' operator\n\n")
+    print("\nNOTE: After EXCLUDE List completion, just hit <Enter to continue generating search output files.\n\n")
+    search_input='None'
+    while search_input != '':
+        search_input=input("\n\nEnter individual search EXCLUDE values, then hit <Enter> to continue: ")
+        excl_list.append(search_input)
+        print("\nYour EXCLUDED search variables will be:\n")
+        for val in excl_list:
+            print(val)
 
 
+    excl_list.pop()
+    val_list.pop()
 
+    if len(excl_list) == 0:
+        excl_list=['!NO_EXCL_VALUE!']
+    if len(val_list) == 0:
+        val_list=['!NO_INCL_VALUE!']
+
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('_%Y-%m-%d_@_%H.%M.%S')
+    filename='inc_'+str(val_list)+'exc_'+str(excl_list)+st+'.xls'
+    rec_filename='rec_'+'inc_'+str(val_list)+'exc_'+str(excl_list)+st+'.xls'
+    out=open(filename,'w')
+    rec=open(rec_filename,'w')
+    print("\n\nWriting output to file: "+filename+'.xls based on search hits on ALL fields in the file input.\n')
+    print("\n\nWriting output to file: "+rec_filename+'.xls based on the search hits on the selected output columns.\n\n')
+
+    for nums in index_list:
+        out.write(col_dict[int(nums)])
+        out.write('\t')
+        rec.write(col_dict[int(nums)])
+        rec.write('\t')
+    out.write('\n')
+    rec.write('\n')
+
+    hit_list=[]
+    stop_list=[]
+    item=1
+
+
+    while item < len_rl:
+        out_list=[]
+        hit_list=[]
+        stop_list=[]
+        for val in val_list:
+            inc=re.search(val,rl[item])
+            hit_list.append(inc)
+        hit_list=list(set(hit_list))
+
+        for ex in excl_list:
+            exc=re.search(ex,rl[item])
+            stop_list.append(exc)
+        stop_list=list(set(stop_list))
+
+
+
+        if hit_list != [None] and stop_list == [None]:
+            field_list=rl[item].split('\t')
+            for idx in index_list:
+                out.write(field_list[int(idx)-1])
+                out.write("\t")
+                out_list.append(field_list[int(idx)-1])
+                len_out_list=len(out_list)
+            out.write("\n")
+
+        for val in val_list:
+            inc=re.search(val,str(out_list))
+            if inc != None:
+                for thing in out_list:
+                    rec.write(thing)
+                    rec.write('\t')
+                rec.write("\n")
+            else:
+                pass
         else:
-            print("\n\nExiting Program\n\n")
-            time.sleep(2)
-            sys.exit()
+            pass
+        item+=1
+
+    out.close()
+    rec.close()
+
+    crunch_file='crunch_'+rec_filename
+    crunch=open(crunch_file,'w')
+    print("\n\nWriting"+crunch_file+"\n\n")
+    print(crunch_file+' eliminates and counts duplicate lines in '+rec_filename)
+    dup_file=open(rec_filename,'r')
+    dup_list=dup_file.readlines()
+    crunch.write(dup_list[0].rstrip('\n')+'\tCount/Occurences\n')
+    del dup_list[0]
+    dup_set=set(dup_list)
+    for item in dup_set:
+        crunch.write(item.rstrip('\n')+'\t'+str(dup_list.count(item)))
+        crunch.write('\n')
+    crunch.close()
+    dup_file.close()
+
+
 
 if __name__=="__main__":
     SNORST()
+    while True:
+        choice=input("\n\n\nWould you like to run the tool again? : [Y] or[N]: ")
+        if choice == 'Y':
+            os.chdir('..')
+            SNORST()
+        elif choice == 'N':
+            print('\n\nGoodbye\n\n')
+            sys.exit()
+        else:
+            print("\n\nPlease enter 'Y or 'N'\n\n")
+            continue
